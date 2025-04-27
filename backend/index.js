4fd665696e9d305ffe3c8d2b6d91fa4deef6850c
ShahilMd <shahil.connect@gmail.com>
2025-04-27 16:08:33 +0530

    feat: integrate Prisma ORM and setup PostgreSQL
    - Add Prisma schema with User models
    - Configure .env for database connection
    - Generate initial migration

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000",
+}));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}); 
